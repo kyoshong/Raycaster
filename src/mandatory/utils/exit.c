35469f01df4de5ce255cd6ca5159c06a86d2965a
kyoshong <qkrgytjd0827@naver.com>
2021-08-12 22:17:08 +0900

    check norm

--- src/mandatory/utils/exit.c
+++ src/mandatory/utils/exit.c
@@ -6,7 +6,7 @@
 /*   By: hyospark <hyospark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 15:58:53 by hyospark          #+#    #+#             */
-/*   Updated: 2021/08/12 21:58:56 by hyospark         ###   ########.fr       */
+/*   Updated: 2021/08/12 22:09:11 by hyospark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,14 @@ void	esc_exit(t_info *info)
 	if (info->img.img)
 	printf("%p\n", info->img.img);
 	mlx_destroy_image(info->mlx, info->img.img);
+	free(info->img.img);
+	printf("%p\n", info->img.img);
+
 		printf("%p\n", info->win);
 	mlx_destroy_window(info->mlx, info->win);
+		// free(info->win);
+
+			printf("%p\n", info->win);
 	cub3d_free_all(info);
 	config_path_free(info->config);
 	free(info->mlx);
