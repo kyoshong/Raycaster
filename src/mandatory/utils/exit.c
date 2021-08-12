2fe8fc812280a9d0727399a25c9c125de58a946b
kyoshong <qkrgytjd0827@naver.com>
2021-08-12 22:19:08 +0900

    check leaks

--- src/mandatory/utils/exit.c
+++ src/mandatory/utils/exit.c
@@ -6,7 +6,7 @@
 /*   By: hyospark <hyospark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 15:58:53 by hyospark          #+#    #+#             */
-/*   Updated: 2021/08/12 22:09:11 by hyospark         ###   ########.fr       */
+/*   Updated: 2021/08/12 22:18:13 by hyospark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ void	esc_exit(t_info *info)
 	config_path_free(info->config);
 	free(info->mlx);
 	free(info->config);
-	system("leaks cub3D > leaks_result_temp;cat leaks_result_temp | grep leaked && rm -rf leaks_result_temp");
+	system("leaks cub3D");
 	exit(0);
 }
